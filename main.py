36974e39165efc355c9f244990906066beae39a2
Yochanan <yochanan.steinberg@gmail.com>
2019-04-05 15:50:21 +0300

    select train and test by datetime

--- main.py
+++ main.py
@@ -2,10 +2,30 @@ import matplotlib.pyplot as plt
 import os
 import pandas as pd
 
-flnm = 'EURUSD-2019-02.csv'
-data = pd.read_csv(os.path.join(r'C:\Users\Yochanan\Downloads\EURUSD-2019-02', flnm))
+filename = 'EURUSD-2019-02.csv'
+full_path = r'C:\Users\Yochanan\Documents\Data\EURUSD'
 
-t = pd.to_datetime(data['datetime'])
+df = pd.read_csv(os.path.join(full_path, filename))
+print(df.columns)
+
+test_date = df['datetime'].values[200000] # '20190201 16:19:50.787'
+print(test_date)
+
+train_d = df.loc[(df.datetime <= test_date)]
+train_d = train_d['high']
+test_d = df.loc[(df.datetime > test_date)]
+test_d = test_d['high']
+
+plt.plot(train_d, label='Train')
+plt.plot(test_d, 'r', label='Test')
+# plt.title('%s adjusted daily close prices' % instrument)
+plt.legend()
+plt.show()
+
+# train_d = low[:test_date]
+# test_d = closes[test_date:]
+
+t = pd.to_datetime(df['datetime'])
 tt = pd.to_timedelta(t)
 tt = tt.dt.total_seconds()
 t = tt - tt[0]
