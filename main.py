fdd393792296c5e3a04edac94469ed87e354aafe
Yochanan <yochanan.steinberg@gmail.com>
2019-04-16 18:14:12 +0300

    plot forecast and test

--- main.py
+++ main.py
@@ -68,11 +68,15 @@ plt.show()
 MAX_LAG_NUMBER = 120 # 4*30 = 1 quarter max
 n_co = ssa_cutoff_order(train_d, dim=MAX_LAG_NUMBER, show_plot=True)
 
-days_to_predict = 15
+samples_to_predict = 60
 forecast = ssa_predict(train_d.values, n_co, list(range(8)), days_to_predict, 1e-5)
 
-prev_ser = closes[datetime.date.isoformat(parser.parse(test_date) - timedelta(120)):test_date]
-plt.plot(prev_ser, label='Train Data')
+# prev_ser = closes[datetime.date.isoformat(parser.parse(test_date) - timedelta(120)):test_date]
+# plt.plot(prev_ser, label='Train Data')
+
+plt.plot(forecast)
+plt.plot(test_d.values[:samples_to_predict])
+
 
 
 f_ser = pd.DataFrame(data=forecast, index=test_d.index[:days_to_predict], columns=['close'])
