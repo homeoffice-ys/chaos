34bd3f278e21593186be2769346df2e09819bf6b
Yochanan <yochanan.steinberg@gmail.com>
2019-04-24 19:54:46 +0300

    todo

--- offline.py
+++ offline.py
@@ -7,6 +7,7 @@ def get_files():
     data_dir = config.config['hist_data_dir']
     if not os.path.isdir(data_dir):
         print('path to csv files not found')
+        print('TODO: hard code fix in config.py file line 4')
         data_dir = input('enter full path to data files: ')
         config.config['hist_data_dir'] = data_dir
     return os.listdir(data_dir)
