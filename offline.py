1fb1acd324a3052806ce1b63d8de20d48a598e4f
Yochanan <yochanan.steinberg@gmail.com>
2019-04-24 19:52:33 +0300

    check data dir

--- offline.py
+++ offline.py
@@ -4,6 +4,11 @@ import config
 
 def get_files():
 
-    return os.listdir(config.config['hist_data_dir'])
+    data_dir = config.config['hist_data_dir']
+    if not os.path.isdir(data_dir):
+        print('path to csv files not found')
+        data_dir = input('enter full path to data files: ')
+        config.config['hist_data_dir'] = data_dir
+    return os.listdir(data_dir)
 
 
