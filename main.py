c50d16dbc94051fb6a5c57d552a4c588f3afb4cf
Yochanan <yochanan.steinberg@gmail.com>
2019-04-28 19:17:41 +0300

    running stats

--- main.py
+++ main.py
@@ -5,10 +5,12 @@ import pandas as pd
 from ssa_core import ssa_main
 import numpy as np
 import config
+from utilities import RunningStats
 from offline import get_files
 
 
 dir_list = get_files()
+rs = RunningStats()
 start_time = None
 
 for f in dir_list:
@@ -19,6 +21,8 @@ for f in dir_list:
         if start_time is None:
             start_time = pd.to_datetime(df.datetime[idx])
         print((pd.to_datetime(df.datetime[idx]) - start_time).total_seconds())
+        rs.push(df['high'].values[idx])
+        print('High = %2.5f, Mean = %2.5f, Std = %2.5f' % (df['high'].values[idx], rs.mean(), rs.standard_deviation()))
         # train_d = df.values
         # test_d = df.values
         # err = ssa_main(train_d, test_d)
